c1cb8b6f2bf2050f6520b17511c16fa7f400ba7e
yannsf29 <yann.crennsf@gmail.com>
2022-03-18 12:29:17 -0700

    design

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import NavBar from "./NavBar";
 import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import "./App.css";
 
 Highcharts.setOptions({
-  colors: ["#058DC7", "#50B432", "#ED561B", "#DDDF00", "#24CBE5"],
+  colors: ["2563eb", "#4f46e5", "#0891b2", "#67e8f9", "#5eead4"],
 });
 
 const dataObject = {
@@ -14,6 +15,8 @@ const dataObject = {
 
 const barOptions = {
   chart: {
+    backgroundColor: "black",
+    borderRadius: 20,
     type: "areaspline",
     height: 400,
   },
@@ -29,6 +32,9 @@ const barOptions = {
     },
   },
   plotOptions: {
+    series: {
+      borderRadius: 5,
+    },
     line: {
       dataLabels: {
         enabled: true,
@@ -41,11 +47,11 @@ const barOptions = {
       type: "bar",
       name: "Taiwan Coinbase",
       data: [
-        { y: dataObject.values[0], color: "#058DC7" },
-        { y: dataObject.values[1], color: "#50B432" },
-        { y: dataObject.values[2], color: "#ED561B" },
-        { y: dataObject.values[3], color: "#DDDF00" },
-        { y: dataObject.values[4], color: "#24CBE5" },
+        { y: dataObject.values[0], color: "#2563eb" },
+        { y: dataObject.values[1], color: "#4f46e5" },
+        { y: dataObject.values[2], color: "#0891b2" },
+        { y: dataObject.values[3], color: "#67e8f9" },
+        { y: dataObject.values[4], color: "#5eead4" },
       ],
     },
   ],
@@ -53,6 +59,8 @@ const barOptions = {
 
 const donutOptions = {
   chart: {
+    backgroundColor: "black",
+    borderRadius: 20,
     plotBackgroundColor: null,
     plotBorderWidth: null,
     plotShadow: false,
@@ -112,28 +120,49 @@ const donutOptions = {
 };
 
 const graphs = [
-  { name: 'Bar', chart:  <HighchartsReact
-  highcharts={Highcharts}
-  options={barOptions}
-  containerProps={{ style: { height: "50%", weight: "50%" } }}
-/> },
-  { name: 'Pie', chart: <HighchartsReact
-  highcharts={Highcharts}
-  options={donutOptions}
-  containerProps={{ style: { height: "50%", weight: "50%" } }}
-/> },
-]
+  {
+    name: "Bar",
+    chart: (
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={barOptions}
+        containerProps={{ style: { height: "50%", weight: "50%" } }}
+      />
+    ),
+  },
+  {
+    name: "Pie",
+    chart: (
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={donutOptions}
+        containerProps={{ style: { height: "50%", weight: "50%" } }}
+      />
+    ),
+  },
+];
 
 const App = () => (
   <>
     <NavBar />
     <div>
-      <h3 className="text-lg leading-6 font-medium text-gray-900 justify-center">Crypto Charts</h3>
+      <h3 className="text-lg leading-6 font-medium text-gray-900 justify-center">
+        <h1 className="text-center">Crypto Charts</h1>
+      </h3>
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
         {graphs.map((item) => (
-          <div key={item.name} className="px-4 py-5 shadow-card bg-red-700 shadow rounded-lg overflow-hidden sm:p-6">
-            <dt className="text-sm font-medium text-yellow-500 truncate">{item.name}</dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900">{item.chart}</dd>
+          <div
+            key={item.name}
+            className="px-4 py-5 shadow-card bg-sky-700 shadow-lg rounded-lg overflow-hidden sm:p-6"
+          >
+            <div className="rounded-lg bg-white p-2">
+              <dt className="text-center font-medium text-decoration-line: overline text-xl text-gray-900 truncate">
+                {item.name}
+              </dt>
+              <dd className="mt-1 text-3xl font-semibold text-gray-900">
+                {item.chart}
+              </dd>
+            </div>
           </div>
         ))}
       </dl>
